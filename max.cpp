e7d4f641c3b60eff09a818f80a7a8eae4a994730
oanh0444@gmail.com <anhnguyetsol97@gmail.com>
2021-04-08 16:48:07 +0700

    Oanh: add new file

---
+++ max.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ 
+int main()
+{
+    float a, b, c;
+    printf("\nNhap 3 so a, b, c: ");
+    scanf("%f%f%f", &a, &b, &c);
+ 
+    // Tìm max theo C1
+    float maxValue = a;
+ 
+    if(b > maxValue){
+        maxValue = b;
+    }
+ 
+    if(c > maxValue){
+        maxValue = c;
+    }
+    printf("max = %f", maxValue);
+    return 0;
+    }
